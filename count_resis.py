84c7d9d14bb34225f548b56d133f0c1e34f9caa5
pgbarletta <pbarletta@gmail.com>
2022-03-30 20:08:35 +0200

    Script to count all the residues (not just the interface) by chain.

--- count_resis.py
+++ count_resis.py
@@ -24,25 +24,7 @@ casa_dir = source_location
 data_dir = Path.joinpath(casa_dir, "data")
 str_dir = Path.joinpath(casa_dir, "structures/raw")
 exposed_dir = Path.joinpath(casa_dir, "structures/exposed")
-# Many PDBs have no interacting light chains, so statistics realitve to the
-# proportion of their interacting polar atoms are skewed since many polar atoms
-# show as noninteracting. 
-def count_ONs_res(topologia, resSeq, chainID):
-    cnt_SC = 0
-    cnt_BB = 0
-    
-    for chain in topologia.chains:
-        if chain.chain_id == chainID:
-            for res in chain.residues:
-                if res.resSeq == resSeq:
-                    for atm in res.atoms:
-                        elemento = atm.element.symbol
-                        is_polar = elemento == 'O' or elemento == 'N'
-                        if is_polar and atm.is_sidechain:
-                            cnt_SC+= 1
-                        elif is_polar and (not atm.is_sidechain):
-                            cnt_BB+= 1
-    return cnt_SC, cnt_BB
+
 
 if __name__ == '__main__':
 
@@ -56,9 +38,10 @@ if __name__ == '__main__':
 
     pdb_list = list(filenames.keys())
     df_dataset = get_df_dataset(casa_dir)
-    count_resi_pdb = {}
-    count_resi = dict(zip(AA_LIST, itertools.repeat(0, len(AA_LIST))))
+    count_resi_inteface_pdb = {}
+    count_resi_inteface = dict(zip(AA_LIST, itertools.repeat(0, len(AA_LIST))))
     count_tot_pdb = {}
+    count_tot_chain_pdb = {}
     count_tot = dict(zip(AA_LIST, itertools.repeat(0, len(AA_LIST))))
     for pdb_idcode in pdb_list:
         print(f"{pdb_idcode}", flush=True)
@@ -77,8 +60,14 @@ if __name__ == '__main__':
             count_tot_this_pdb[key] = val
             count_tot[key] += val
         count_tot_pdb[pdb_idcode] = count_tot_this_pdb
+
+        cnt_ag = len([residue for residue in trj_in.topology.residues\
+            if residue.chain.chain_id in chains[pdb_idcode].antigen ])
+        cnt_ab = len([residue for residue in trj_in.topology.residues\
+            if residue.chain.chain_id in chains[pdb_idcode].antibody ])
+        count_tot_chain_pdb[pdb_idcode] = ResiCount(antibody=cnt_ab, antigen=cnt_ag)
         ###
-        # ANTIBODY
+        # ANTIBODY INTERFACE
         ###
         interface_ab = []
         for linea in df_interface.query(f"idcode == '{pdb_idcode}'").ab_ag_interface_res.values[0]:
@@ -95,7 +84,7 @@ if __name__ == '__main__':
             count_res_ab[resname] += 1
 
         ###
-        # ANTIGEN
+        # ANTIGEN INTERFACE
         ###
         interface_ag = []
         for linea in df_interface.query(f"idcode == '{pdb_idcode}'").ag_ab_interface_res.values[0]:
@@ -111,19 +100,21 @@ if __name__ == '__main__':
             resname = unique_[1:4]
             count_res_ag[resname] += 1
 
-        count_resi_pdb[pdb_idcode] = ResiCount(antibody=count_res_ab, antigen=count_res_ag)
-        for key in count_resi.keys():
-            count_resi[key] += count_res_ab[key]
-            count_resi[key] += count_res_ag[key]
+        count_resi_inteface_pdb[pdb_idcode] = ResiCount(antibody=count_res_ab, antigen=count_res_ag)
+        for key in count_resi_inteface.keys():
+            count_resi_inteface[key] += count_res_ab[key]
+            count_resi_inteface[key] += count_res_ag[key]
         
-    with open(Path.joinpath(casa_dir, 'data', 'count_resi_pdb.pkl'), 'wb') as file:
-        pickle.dump(count_resi_pdb, file)
-    with open(Path.joinpath(casa_dir, 'data', 'count_resi.pkl'), 'wb') as file:
-        pickle.dump(count_resi, file)
-    with open(Path.joinpath(casa_dir, 'data', 'count_tot_pdb.pkl'), 'wb') as file:
-        pickle.dump(count_tot_pdb, file)
-    with open(Path.joinpath(casa_dir, 'data', 'count_tot.pkl'), 'wb') as file:
-        pickle.dump(count_tot, file)
+    # with open(Path.joinpath(casa_dir, 'data', 'count_resi_inteface_pdb.pkl'), 'wb') as file:
+    #     pickle.dump(count_resi_inteface_pdb, file)
+    # with open(Path.joinpath(casa_dir, 'data', 'count_resi_inteface.pkl'), 'wb') as file:
+    #     pickle.dump(count_resi_inteface, file)
+    # with open(Path.joinpath(casa_dir, 'data', 'count_tot_pdb.pkl'), 'wb') as file:
+    #     pickle.dump(count_tot_pdb, file)
+    with open(Path.joinpath(casa_dir, 'data', 'count_tot_chain_pdb.pkl'), 'wb') as file:
+        pickle.dump(count_tot_chain_pdb, file)
+    # with open(Path.joinpath(casa_dir, 'data', 'count_tot.pkl'), 'wb') as file:
+    #     pickle.dump(count_tot, file)
 
 
 
